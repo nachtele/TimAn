c45fa321f0ec984bd787c8de71c927c6f5a43987
Nachtele <nachtele@gmail.ccom>
2023-02-19 16:23:58 +0900

    TimAnMakeFig.py: 水平カーソルの閾値表示を追加。main波形上の文字のアラインメントを'right'に変更。

--- src/TimAnMakeFig.py
+++ src/TimAnMakeFig.py
@@ -44,22 +44,23 @@ class makeFig:
         for name, tv, str, tr, vp in strSettings:
             for t, v in zip(ta.ht[name], ta.hv[name]):
                 if v == tv:
-                    self.ax[0][tr].annotate(str, xy=(t, vp), xytext=(t, vp))
+                    self.ax[0][tr].annotate(str, xy=(t, vp), xytext=(t, vp), ha='right')
     def hlines(self, ch):
         for anp in self.hllist: anp.remove()
         self.hllist[:] = []
         for [c, th] in ch:
             self.hllist.append(self.ax[1][c].axhline(y=th, xmin=0, xmax=1, color='gray', alpha=0.5))
         pass
-    def ann(self, ch, v, tg, lny, vly, vty):
+    def ann(self, ch, v, tg, tas, lblx):
         for anp in self.aplist: anp.remove()
         self.aplist[:] = []
-        for [c, th], ly, t in zip(ch, lny, tg):
+        for [c, th], ly, t in zip(ch, tas.lny, tg):
+            self.aplist.append(self.ax[1][c].annotate(f'{th:.2f}', xy=(lblx, th), xytext=(4, 0), va='center', textcoords='offset pixels'))
             self.aplist.append(self.ax[2][0].annotate(engFmt(t), xy=(t, ly[0]), xytext=(t, ly[1]), annotation_clip=False, arrowprops=ap))
             for ax in self.ax[1][c:]:
                 self.aplist.append(ax.axvline(x=t, ymin=0, ymax=1, color='gray', alpha=0.5))
-        self.aplist.append(self.ax[2][0].annotate(engFmt(v), xy=(tg[1], vty), xytext=(tg[1], vty)))
-        patch = patches.FancyArrow(x=tg[1], y=vly, dx=v, dy=0, width=0, head_width=0.1, head_length=0.05*v, length_includes_head=True)
+        self.aplist.append(self.ax[2][0].annotate(engFmt(v), xy=(tg[1], tas.vty), xytext=(tg[1], tas.vty)))
+        patch = patches.FancyArrow(x=tg[1], y=tas.vly, dx=v, dy=0, width=0, head_width=0.1, head_length=0.05*v, length_includes_head=True)
         self.ax[2][0].add_patch(patch)
         self.aplist.append(patch)
     def zoom(self, xmin, xmax):
@@ -111,7 +112,7 @@ def plotTa(tim, dat, ta, tas, chs, zs, dstDir):
             xmax = (tg[0] + tg[1] + z) / 2
             xmin = xmax - z
             afig.zoom(xmin, xmax)
-            afig.ann(ch, v, tg, tas.lny, tas.vly, tas.vty)
+            afig.ann(ch, v, tg, tas, xmax)
             if tas.mainZoom:
                 for mzmin, mzmax in tas.mainZoom:
                     if mzmin <= tg[0] and tg[1] <= mzmax:
